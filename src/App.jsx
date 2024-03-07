4f5851cebee07f97ff07bc853cbf0e9d89abad86
jonathan correa <jonathancorrea291292@hotmail.com>
2024-03-07 19:35:40 -0300

    instalacion de tailwinds

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import ListadoPacientes from "./components/ListadoPacientes";
-
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
 function App() {
     return (
         <>
