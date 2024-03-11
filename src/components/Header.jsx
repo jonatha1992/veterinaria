55350955a864c223f5d2b7cc62bb0b4d06b365b4
jonathan correa <jonathancorrea291292@hotmail.com>
2024-03-10 23:29:24 -0300

    cambios en el titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className="mt-5">
-            <h1 className="font-black text-5xl text-center w-2/4 mx-auto">
+            <h1 className="font-black text-5xl text-center lg:w-2/4  md:w-3/4 mx-auto">
                 Seguimiento Pacientes <strong className=" text-indigo-500">Veterinarias</strong>{" "}
             </h1>
         </div>
